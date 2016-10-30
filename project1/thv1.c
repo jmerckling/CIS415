cdcbd109aecad151e6b1f92322dce23e63d3fc7f
jmerckling <jmerckling94@gmail.com>
2016-10-30 14:49:16 -0700

    thv1 (incomplete)

--- project1/thv1.c
+++ project1/thv1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#inlude <unistd.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include "p1fxns.h"
 
@@ -16,25 +16,30 @@ int main(int argc,char *argv[])
     int nprocesses;
     char buf[BUF_SIZE];
     struct timeval start, stop;
-    char *arg[MAX_ARGS]
-    char command[BUF_SIZE];
-    pid_t pid[MAX_PIDS]
+    char *arg[MAX_ARGS];
+    char* command;
+    pid_t pid[MAX_PIDS];
 
     int x;
     for(i = 1; i <= argc; i ++)
         {
-            if(p1strneq(argv[i], "--command=", 10))
+           	if(p1strneq(argv[i], "--command=",p1strlen("--command=")))
                 {
-                   buf = argv[i]+p1strlen("--command=");
-                   p1strcpy(buf, argv[i]+plstrlen("--command="));
+                  // need to set allocation
+		    command = (char *) malloc((p1strlen(argv[i] + 1))*sizeof(char));
+                   p1strcpy(command, argv[i] + p1strlen("--command="));
+		   printf("Command: %s", command);
                 }
-        }
+		else if(p1strneq(argv[i], "--number=",  p1strlen("--number=")))
+		{
+		    nprocesses = p1atoi(argv[i] + p1strlen("--number="));
+		}
+		else if(p1strneq(argv[i], "--processors=", p1strlen("--processors=")))
+		{
+		    nprocessors = p1atoi(argv[i] + p1strlen("--processors="));			    
+		}    
 
-    /*if(argc >= 2)
-	{
-	    if(p1strneq(argv[argc-1], "--command=", 10) != 1)
-	    {
-	        			
-	    }
-    	}
-}	*/	
+        }
+    free(command);
+}//end main
+ 	

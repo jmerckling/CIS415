0f18ad8476be52f700c5baf2e14c61ec694b86f0
jmerckling <jmerckling94@gmail.com>
2016-10-31 16:00:21 -0700

    thv1 practially done

--- project1/thv1.c
+++ project1/thv1.c
@@ -19,11 +19,14 @@ int main(int argc,char *argv[])
     int i;
     int nprocessors;
     int nprocesses;
-    char buf[BUF_SIZE];
+    char *buf[BUF_SIZE];
+    char word[MAX_ARGS];
     struct timeval start, stop;
-    char *arg[MAX_ARGS];
+   // char **arg[MAX_ARGS];
     char* command;
     pid_t pid[MAX_PIDS];
+    int proc_nums = 0;
+    int proc_pro = 0;
 
     for(i = 1; i < argc; i++)
         {
@@ -37,22 +40,43 @@ int main(int argc,char *argv[])
 			else if(p1strneq(argv[i], "--number=",  p1strlen("--number=")))
 			{
 		    	nprocesses = p1atoi(argv[i] + p1strlen("--number="));
+			proc_nums = 1;
 				//printf("Processes: %d\n", nprocesses);//delete
 			}
 			else if(p1strneq(argv[i], "--processors=", p1strlen("--processors=")))
 			{
 		   	 	nprocessors = p1atoi(argv[i] + p1strlen("--processors="));
+				proc_pro = 1;
 				//printf("Processors: %d\n", nprocessors);//delete			    
-			}    
-        }
+			}
+	}
+
+		int j = 0;
+
+		char **arg = malloc(sizeof(char*) * (p1strlen(command)+1));
+		for(i = 0; i < p1strlen(command) + 1; i++)
+		{
+		    arg[i] = malloc(sizeof(char) * 100);
+		}
+		int command_location = 0;
+
+		command_location = p1getword(command, command_location, arg[j]);
+
+		while(command_location != -1)
+		    {
+			j++;
+			command_location = p1getword(command, command_location, arg[j]);
+		    }
+		free(arg[j]);
+		arg[j] = NULL;
 		
 		//if no argument is given for processes set it to TH_NPROCESSES
-		if(nprocessors < 1)
+		if(proc_pro == 0)
 		{
 			char *x;		
             if ((x = getenv("TH_NPROCESSORS")) != NULL)
                { 
-				   printf("%s\n", x);
+				  // printf("%s\n", x);
 				   nprocessors = p1atoi(x);
 			   }
 			else
@@ -62,12 +86,12 @@ int main(int argc,char *argv[])
 				}
 		}
 		//if no argument is given for processes set it to TH_NPROCESSES
-		if(nprocesses < 1)
+		if(proc_nums == 0)
 		{
 			char *x;		
             if ((x = getenv("TH_NPROCESSES")) != NULL)
                { 
-				   printf("%s\n", x);
+				   //printf("%s\n", x);
 				   nprocesses = p1atoi(x);
 			   }
 			else
@@ -83,24 +107,32 @@ int main(int argc,char *argv[])
 			pid[i] = fork();
 			if(pid[i] == 0)
 			{
-				execvp(arg[0], arg);
+			   execvp(arg[0], arg);
 			}
 		}
 		
 		for(i = 0; i < nprocesses; i++)
 		{
-			system(command);
 			int status;
 			waitpid(pid[i], &status, 0);
 		}
 		
-		//printf("Command: %s\n", command);//delete
+	    	//printf("Command: %s\n", arg[0]);//delete
 		//printf("Processes: %d\n", nprocesses);//delete
 		//printf("Processors: %d\n", nprocessors);//delete
 		gettimeofday(&stop, NULL);
-		double t = stop.tv_usec - start.tv_usec;
-		printf("The elapsed time to execute %d copies of \\”%s\\” on %d processors is: %7.3fmicrosec\n", nprocesses, command, nprocessors, t);
-		//write()
+		float t = (((stop.tv_sec - start.tv_sec) * 1000000L + stop.tv_usec) - start.tv_usec)/1000000.0;
+	//	printf("The elapsed time to execute %d copies of \"%s\" on %d processors is: %7.3fsec\n", nprocesses, command, nprocessors, t);
+		//display without printf
+		p1putstr(1, "The elapsed time to execute ");
+		p1putint(1, nprocesses);
+		p1putstr(1, " copies of \"");
+		p1putstr(1, command);
+		p1putstr(1, "\" on ");
+		p1putint(1, nprocessors);
+		p1putstr(1, " processors is: ");
+		p1putint(1, t);
+		p1putstr(1, "sec\n");
 		
 		
     free(command);

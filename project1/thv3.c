33ee03b30d2a807bd30f0d4d2b7a4e22c7ef9929
jmerckling <jmerckling94@gmail.com>
2016-11-02 20:03:09 -0700

    thv3 so far

--- project1/thv3.c
+++ project1/thv3.c
@@ -2,7 +2,6 @@
 //CIS 415 Project 1
 //This is my own work
 
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/wait.h>
@@ -22,7 +21,7 @@ pid_t deadch[BUF_SIZE];
 int killcount;
 int nprocessors;
 int nprocesses;
-pid_t *pid;
+pid_t pid[MAX_ARGS];
 
 
 
@@ -33,31 +32,11 @@ void USR1_handler(UNUSED int signo)
     USR1_received++;
 }
 
-/*void ALRM_handler(UNUSED int signo)
-{
-    kill(active->pid, SIGSTOP);
-    printf("alarm going off\n"); // test delete
-    ALRM_received++;
-}
-*/
-/*void SIGCHLD_handler(UNUSED int signo)
-{
-    
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) 
-    {
-	if (WIFEXITED(status) || WIFSIGNALED(status)) 
-	{ 
-	    killcount++;
-	}
-    }
-}
-*/
 typedef struct processor
 {
     pid_t pid;
     struct processor *next;
     int status;
-    //Processor *previous;
 } pr;
 
 typedef struct queue
@@ -78,64 +57,64 @@ void ALRM_handler(UNUSED int signo)
     int i;
     for(i = 0; i < nprocesses; i++)
     {
-	pr* ptemp = malloc(sizeof(pr));
-	ptemp->pid = pid[i];
-	ptemp->status = 0;
-	enqueue(&wQ, ptemp);
-	free(ptemp);
+        pr* ptemp = malloc(sizeof(pr));
+        ptemp->pid = pid[i];
+        ptemp->status = 0;
+        enqueue(&wQ, ptemp);
+        free(ptemp);
     }
 
     for(i = 0; i < nprocesses; i++)
     {
-	if(wQ.size == 0)
-	    p1perror(2, "wait queue empty");
-	pr* t = deq(&wQ);
-	kill(pid[i], SIGUSR1);
-	if(t->status == 0)
-	{
-	    t->status = 1;
-	    enqueue(&rQ, t);
-	}
-	else
-	{
-	    kill(pid[i], SIGCONT);
-	}
+        if(wQ.size == 0)
+            p1perror(2, "wait queue empty");
+        pr* t = deq(&wQ);
+        kill(pid[i], SIGUSR1);
+        if(t->status == 0)
+        {
+            t->status = 1;
+            enqueue(&rQ, t);
+        }
+        else
+        {
+            kill(pid[i], SIGCONT);
+        }
     }
 
     int f = 0;
     for(i = 0; i < rQ.size; i++)
     {
-	pr *node = deq(&rQ);
-	int k;
-	for(k = 0; k < killcount; k++)
-	{
-	    if(deadch[k] == node->pid)
-	    {
-		f = 1;
-	    }
-	}
-	if(f == 0)
-	{
-	    enqueue(&wQ, node);
-	}
-	else
-	{
-	    free(node);
-	}
+        pr *node = deq(&rQ);
+        int k;
+        for(k = 0; k < killcount; k++)
+        {
+            if(deadch[k] == node->pid)
+            {
+                f = 1;
+            }
+        }
+        if(f == 0)
+        {
+            enqueue(&wQ, node);
+        }
+        else
+        {
+            free(node);
+        }
     }
 }
 
-static void CHLD_handler(UNUSED int sig) 
-{ 
-    pid_t pid; int status;
-	while ((pid = waitpid(-1, &status, WNOHANG)) > 0) 
-	{
-	    if (WIFEXITED(status) || WIFSIGNALED(status)) 
-	    { 
-		    killcount++; 
-		    fprintf(stderr, "%d: exited\n", pid); 
-	    } 
-	} 
+static void CHLD_handler(UNUSED int signo)
+{
+    pid_t pid;
+    int status;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
+    {
+        if (WIFEXITED(status))
+        {
+            killcount++;
+        }
+    }
 }
 
 void enqueue(Queue* queue, pr *process)
@@ -145,13 +124,13 @@ void enqueue(Queue* queue, pr *process)
     {
         queue->head = cur;
         queue->tail = cur;
-	queue->size = queue->size+1;
+        queue->size = queue->size+1;
         return;
     }
     else
         queue->tail->next = cur;
-	queue->tail = cur;
-	queue->size = queue->size+1;
+    queue->tail = cur;
+    queue->size = queue->size+1;
 }
 
 pr* deq(Queue *queue)
@@ -163,7 +142,7 @@ pr* deq(Queue *queue)
     else if (queue->head == queue->tail)
     {
         pr *cur = queue->head;
-	queue->size = queue->size-1;
+        queue->size = queue->size-1;
         queue->head = NULL;
         queue->tail = NULL;
         return cur;
@@ -172,21 +151,17 @@ pr* deq(Queue *queue)
     {
         pr *cur = queue->head;
         queue->head = cur->next;
-	queue->size = queue->size-1;
+        queue->size = queue->size-1;
         return cur;
-
     }
 }
 
 int main(int argc,char *argv[])
 {
     int i;
-    //int nprocessors;
-    //int nprocesses;
     struct timeval start, stop;
     struct itimerval it_val;
     char* command;
-    //pid_t pid[MAX_PIDS];
     int proc_nums = 0;
     int proc_pro = 0;
 
@@ -214,19 +189,16 @@ int main(int argc,char *argv[])
             // need to set allocation
             command = (char *) malloc((p1strlen(argv[i] + 1))*sizeof(char));
             p1strcpy(command, argv[i] + p1strlen("--command="));
-            //printf("Command: %s\n", command);//delete
         }
         else if(p1strneq(argv[i], "--number=",  p1strlen("--number=")))
         {
             nprocesses = p1atoi(argv[i] + p1strlen("--number="));
             proc_nums = 1;
-            //printf("Processes: %d\n", nprocesses);//delete
         }
         else if(p1strneq(argv[i], "--processors=", p1strlen("--processors=")))
         {
             nprocessors = p1atoi(argv[i] + p1strlen("--processors="));
             proc_pro = 1;
-            //printf("Processors: %d\n", nprocessors);//delete
         }
     }
     if(command == NULL)
@@ -242,8 +214,8 @@ int main(int argc,char *argv[])
     {
         arg[i] = malloc(sizeof(char) * 100);
     }
-    int command_location = 0;
 
+    int command_location = 0;
     command_location = p1getword(command, command_location, arg[j]);
 
     while(command_location != -1)
@@ -260,7 +232,6 @@ int main(int argc,char *argv[])
         char *x;
         if ((x = getenv("TH_NPROCESSORS")) != NULL)
         {
-            // printf("%s\n", x);
             nprocessors = p1atoi(x);
         }
         else
@@ -275,7 +246,6 @@ int main(int argc,char *argv[])
         char *x;
         if ((x = getenv("TH_NPROCESSES")) != NULL)
         {
-            //printf("%s\n", x);
             nprocesses = p1atoi(x);
         }
         else
@@ -284,129 +254,35 @@ int main(int argc,char *argv[])
             _exit(1);
         }
     }
-    pid[nprocesses];
     gettimeofday(&start, NULL);
 
-        /*for(i = 0; i < nprocesses; i++)
+    for(i = 0; i < nprocesses; i++)
+    {
+        setitimer(ITIMER_REAL, &it_val,NULL);
+        if(setitimer(ITIMER_REAL, &it_val, NULL) == -1)
         {
-    	if((pid[i] = fork()) < 0)
-    	    {
-    		p1perror(2, "Fork unsuccessful\n");
-    		_exit(errno);
-    	    }
-    	if(pid[i] == 0)
-    	    {
-    		while(!USR1_received)
-    		    {
-    			pause();
-    		    }
-    	    }
+            return 1;
         }
-        */
-    //p->pid = fork();
-    //p->next = NULL;
-
-    for(i = 0; i < nprocesses; i++)
-    {
         pid[i] = fork();
-    }
-
-    if(setitimer(ITIMER_REAL, &it_val, NULL) == -1)
-    {
-	p1perror(2,"error calling setitimer()"); 
-	return 1;
-    }
-
-    /*for(i = 0; i < nprocesses; i++)
-    {
-	pr* ptemp = malloc(sizeof(pr));
-	ptemp->pid = pid[i];
-	ptemp->status = 0;
-	enqueue(&wQ, ptemp);
-	free(ptemp);
-    }
-
-    for(i = 0; i < nprocesses; i++)
-    {
-	if(wQ.size == 0)
-	    p1perror(2, "wait queue empty");
-	pr* t = deq(&wQ);
-	kill(pid[i], SIGUSR1);
-	if(t->status == 0)
-	{
-	    t->status = 1;
-	    enqueue(&rQ, t);
-	}
-	else
-	{
-	    kill(pid[i], SIGCONT);
-	}
-    }
-
-    int f = 0;
-    for(i = 0; i < rQ.size; i++)
-    {
-	pr *node = deq(&rQ);
-	int k;
-	for(k = 0; k < killcount; k++)
-	{
-	    if(deadch[k] == node->pid)
-	    {
-		f = 1;
-	    }
-	}
-	if(f == 0)
-	{
-	    enqueue(&wQ, node);
-	}
-	else
-	{
-	    free(node);
-	}
-    }
-*/
-    for(i = 0; i < nprocesses; i++)
-    {
         if(pid[i] == 0)
         {
-            while(! USR1_received)//wait until USR1 is received
-            {
-                sleep(1);
-            }
+
             execvp(arg[0], arg);
         }
     }
 
-    /*for(i = 0; i < nprocesses; i++)
-    {
-    	int status;
-    	waitpid(pid[i], &status, 0);
-    }
-    */
-    /*for(i = 0; i < nprocesses; i++)
-    {
-        kill(pid[i], SIGUSR1);
-    }
-    for(i = 0; i < nprocesses; i++)
-    {
-        //sleep(3);//test pause
-        kill(pid[i], SIGSTOP);
-    }
-    for(i = 0; i < nprocesses; i++)
-    {
-        //sleep(3);//test pause
-        kill(pid[i], SIGCONT);
-    }
     for(i = 0; i < nprocesses; i++)
     {
         waitpid(pid[i], NULL, 0);
     }
-*/
+
+    char ms[10];
+    ms[0] = 0;
     gettimeofday(&stop, NULL);
     float t = (((stop.tv_sec - start.tv_sec) * 1000000L + stop.tv_usec) - start.tv_usec)/1000000.0;
-
-    //	printf("The elapsed time to execute %d copies of \"%s\" on %d processors is: %7.3fsec\n", nprocesses, command, nprocessors, t);
-    //display without printf
+    int perLeft = (((stop.tv_sec - start.tv_sec)*1000000L + stop.tv_usec) - start.tv_usec)/1000000;
+    int perRight = (((stop.tv_sec - start.tv_sec)*1000000L + stop.tv_usec) - start.tv_usec)/1000 - perLeft * 1000;
+    
     p1putstr(1, "The elapsed time to execute ");
     p1putint(1, nprocesses);
     p1putstr(1, " copies of \"");
@@ -414,7 +290,18 @@ int main(int argc,char *argv[])
     p1putstr(1, "\" on ");
     p1putint(1, nprocessors);
     p1putstr(1, " processors is: ");
-    p1putint(1, t);
+    p1putint(1, perLeft);
+    p1putstr(1, ".");
+    if(perRight < 100)
+    {
+    p1putstr(1,(char*)"0");
+	if(perRight < 10)
+	{
+	    p1putstr(1, (char*)"0");
+	}
+    }
+    p1itoa(perRight, ms);
+    p1putstr(1,ms);
     p1putstr(1, "sec\n");
 
 
